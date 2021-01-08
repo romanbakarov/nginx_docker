d0504ec2a054ecb5810897375fafbf21d7280fda
romanbakarov <roman.bakarov@starnavi.io>
2021-01-08 12:50:23 +0200

    add conf for static and media

--- starnavi_test_task/starnavi_test_task/settings.py
+++ starnavi_test_task/starnavi_test_task/settings.py
@@ -122,7 +122,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = "/static"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/media"
 
 DATETIME_INPUT_FORMATS = ['%Y-%m-%d %H:%M:%S']
 
--- starnavi_test_task/starnavi_test_task/urls.py
+++ starnavi_test_task/starnavi_test_task/urls.py
@@ -1,8 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from media_test.views import image_upload
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('social_network_api.urls')),
     path('api-auth/', include('accounts.urls', namespace='accounts')),
+    path("upload/", image_upload, name="upload"),
 ]
+
+if bool(settings.DEBUG):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
